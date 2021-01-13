c6a9928ee353186975c2a73e28697a93cbd97e56
Jakidxav <jakidxav@protonmail.com>
2021-01-13 12:36:09 +0100

    adding setup.py script to repository so that packages can be imported from any folder

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(name     = 'avalanche_ml',
+      #author   = '',
+      #description = ''
+      version  = '0.7',
+      license  = 'MIT',
+      url      = 'https://github.com/NVE/avalanche_ml',
+
+      packages = ['analysis',
+                  'avaml',
+                  'avaml.machine',
+                  'modeling'],
+
+      install_requires = ['numpy>1.17.0', 'pandas>1.0.0', 'matplotlib>3.0.0']
+     )
