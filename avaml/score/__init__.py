ade2664ac33e2bdb34eaebd7d2f2ab48340d0a63
Aron Widforss <arwi@nve.no>
2021-01-20 17:09:03 +0100

    Use spatial_diff instead of overlap
    Since all other values in the Score metric should be interpreted
    as lower is better, we define spatial_diff = 1 - overlap. This make
    the values in Score.calc()'s returned DataFrame easier to parse.

--- avaml/score/__init__.py
+++ avaml/score/__init__.py
@@ -93,13 +93,13 @@ class Score:
     def calc(self):
         diff_cols = [not re.match(r"^(lev_)|(aspect)", col) for col in self.label_vectors.columns.get_level_values(1)]
         diff = self.pred_vectors.loc[:, diff_cols] - self.label_vectors.loc[:, diff_cols]
-        p_score_cols = pd.MultiIndex.from_tuples([(_NONE, "p_score")]).append(
-            pd.MultiIndex.from_product([[f"problem_{n}" for n in range(1, 4)], ["overlap"]])
+        p_score_cols = pd.MultiIndex.from_tuples([(_NONE, "problem_score")]).append(
+            pd.MultiIndex.from_product([[f"problem_{n}" for n in range(1, 4)], ["spatial_diff"]])
         )
         p_score = pd.DataFrame(index=diff.index, columns=p_score_cols)
         for idx, series in self.label_vectors.iterrows():
-            problem_score, overlaps = dist(series, self.pred_vectors.loc[idx])
-            p_score.loc[idx] = np.array([problem_score] + overlaps)
+            problem_score, spatial_diffs = dist(series, self.pred_vectors.loc[idx])
+            p_score.loc[idx] = np.array([problem_score] + spatial_diffs)
         weights = np.array([1, 1, 1])
         maxdist = np.power(weights, 2).sum()
         score = pd.DataFrame(
@@ -114,18 +114,18 @@ class Score:
 
 def dist(score1, score2):
     def distvec(vec1, vec2):
-        overlap = calc_overlap(vec1, vec2)
+        spatial_diff = 1 - calc_overlap(vec1, vec2)
 
         # Distance to the null forecast is the distance to the plane freq = 0.
         if not vec1["freq"]:
-            return vec2["freq"], overlap
+            return vec2["freq"], spatial_diff
         if not vec2["freq"]:
-            return vec1["freq"], overlap
+            return vec1["freq"], spatial_diff
 
-        diff = np.append(vec1.iloc[:3] - vec2.iloc[:3], [1 - overlap])
+        diff = np.append(vec1.iloc[:3] - vec2.iloc[:3], [spatial_diff])
         # With two valid forecasts, compute a weighted euclidean norm.
         norm = np.linalg.norm((diff) * np.array([1, 1, 1, 0.5]))
-        return norm, overlap
+        return norm, spatial_diff
 
     def mindist(vec1, vecs):
         mindist = None
@@ -139,13 +139,13 @@ def dist(score1, score2):
     distance = 0
     prob_cols = [f"problem_{n}" for n in range(1, 4)]
     weights = [3, 2, 1]
-    overlaps = []
+    spatial_diffs = []
     for prob_n, weight in zip(prob_cols, weights):
-        norm, overlap = distvec(score1[prob_n], score2[prob_n])
+        norm, spatial_diff = distvec(score1[prob_n], score2[prob_n])
         distance += norm * weight
         dist_1 = mindist(score1[prob_n], [score2[prob_m] for prob_m in prob_cols])
         dist_2 = mindist(score2[prob_n], [score1[prob_m] for prob_m in prob_cols])
         distance += (dist_1 + dist_2) * weight / 2
-        overlaps.append(overlap)
+        spatial_diffs.append(spatial_diff)
 
-    return distance / (2 * maxdist * sum(weights)), overlaps
+    return distance / (2 * maxdist * sum(weights)), spatial_diffs
